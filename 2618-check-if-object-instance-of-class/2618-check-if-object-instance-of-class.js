2002443c5960ad720ae2cdfe999bd21f7a82f8f6
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-01-20 12:17:30 -0600

    2618-check-if-object-instance-of-class .js 63 ms (92.72%) 61.9 MB (96.19%)

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,24 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false;
+    }
+
+    let currPrototype = Object.getPrototypeOf(obj);
+    while (currPrototype !== null) {
+        if (currPrototype === classFunction.prototype) {
+            return true;
+        }
+        currPrototype = Object.getPrototypeOf(currPrototype); 
+    }
+
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
